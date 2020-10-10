10ce6ef4a5c096286a9550b40b49d0444a68ebf1
Paymon Jalali <paymon.jalali@gmail.com>
2020-10-10 12:16:50 -0700

    adding exception handling

--- student.cpp
+++ student.cpp
@@ -1,7 +1,13 @@
 #include "student.h"
+#include <stdexcept>
 
 Student::Student(unsigned int studentNo, float percent) 
-    : studentNumber(studentNo), grade(percent) {}
+    : studentNumber(studentNo), grade(percent)
+{
+    if (percent < 0 || percent > 100) {
+        throw std::invalid_argument("Invalid Argument Exception: grade must be between 0 and 100 inclusively.");
+    }
+}
 
 unsigned int Student::getStudentNumber()
 {
